90f3a59c3c0161961e0980a9a70a6a05a45f299c
nathan lachenmyer <nathan@smalldesignfirm.com>
2016-04-27 00:16:32 -0400

    switched to vector

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,21 +3,16 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	//mFbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
-	//mFbo.begin();
-	ofBackground(0);
-
-//	ofImage img;
 	output.beginEPS("invaders.eps");
 	output.fill();
 
 	uint16_t id = 0;
 
-	for (int i = 0; i < 257; i++) {
-		for (int j = 0; j < 255; j++) {
+	for (int i = 0; i < 80; i++) {
+		for (int j = 0; j < 60; j++) {
 			//std::printf("Generating invader of id %d\n", id);
-			mInvader = new Invader(id, 5, 100, &output);
-			mInvader->draw(i*100, j*100);
+			mInvader = new Invader(id, 5, 10, &output);
+			mInvader->draw(i*10, j*10);
 			id++;
 			delete mInvader;
 		}
@@ -25,14 +20,6 @@ void ofApp::setup(){
 
 	output.endEPS();
 
-	//mFbo.end();
-	//mFbo.draw(0, 0);
-	//img.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
-	//std::ostringstream filepath;
-	//filepath << ofGetFrameNum() << ".png";
-//	std::string filepath = "all_invaders.png";
-//	mFbo.readToPixels(mPixels);
-//	ofSaveImage(mPixels, filepath, OF_IMAGE_QUALITY_BEST);
 	std::printf("Saving complete!\n");
 }
 
@@ -43,7 +30,6 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	//exit();
 }
 
 //--------------------------------------------------------------
